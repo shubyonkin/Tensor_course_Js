b1a63954a495c53040689328ad6a503d25a9e41d
UserX <schubenkin00@gmail.com>
2020-05-31 21:45:04 +0200

    les 10
    les 10

--- Public/js/Person.js
+++ Public/js/Person.js
@@ -1,17 +1,17 @@
 import {Component,PopupList} from './componentLib.js';
 
 export class Person extends Component {
-    render({item}) {
+    render(item) {
        return `<div class="card card_person">
            <img class="card__img card__img_round" src="${item.photo || 'img/ui/default_pix.jpg'}" alt="Аватар ${item.title}" />
-           <p class="card__title" title="${item.title || ''}">${item.title || ''}</p>
+           <p class="card__title" title="${item.title || ''}">${item.title || 'ничего не вылезло'}</p>
            <span class="card__description" title="${item.study || ''}">${item.study || ''}</span>
        </div>`;
     }
  
-    afterMount() {
-       this.container.addEventListener('click', (event) => this.onClick(event) );
-    }
+    //afterMount() {
+       //this.container.addEventListener('click', (event) => this.onClick(event) );
+    //}
     
     onClick(event) {
        if (!this.popup) {
--- Public/js/app.js
+++ Public/js/app.js
@@ -19,48 +19,102 @@ class Model {
    constructor(data) {
       for(let key in data) {
          this[key] = data[key];
+        
       }
+      this.type = 'model';
    }
 
-   get fullName() {
-      return `{this.title}`;
+   get_fullName() {
+      return `${this.title}+1`;
    }
 }
 
-let personModelList = 
-[new Model({
-   title: 'Женя Серова',
-   photo: 'img/ava03.jpg',
-   study: 'Угату',
-   bday: new Date('1998-11-13'),
-   phone: '+7 (963) 123-45-67',
-   active: new Date('2020-04-03T20:00:00'),
-   popupStack: stack 
-}),
-new Model({
-   title: 'Полина Гришина',
-   photo: 'img/ava02.jpg',
-   study: 'Угату',
-   bday: new Date('1998-11-13'),
-   phone: '+7 (963) 123-45-67',
-   active: new Date('2020-04-03T20:00:00'),
-   popupStack: stack 
-}),
-new Model({
-   title: 'Дмитрий Дмитриевич',
-   photo: 'img/ava01.jpg',
-   study: 'Угату',
-   bday: new Date('1998-11-13'),
-   phone: '+7 (963) 123-45-67',
-   active: new Date('2020-04-03T20:00:00'),
-   popupStack: stack 
-}),
-
-];
 
 
-for(let i=0;i<personModelList.length;i++){
-   list.add(personModelList[i]);
+class DataSet{
+   constructor(options){
+      this.options = {
+         ...{
+      host: 'http://localhost:8080/api/',
+      model: Model
+         },
+         ...options
+      };
+   }
+   toModel(result){
+      //console.log(`${this.options.model}  создался`)
+      return new Model(result);
+   }
+
+   query(query,options,params){
+      let url  = new URL(this.options.host);
+      url.pathname += query;
+      for (let k in params){
+         url.searchParams.set(k,params[k]);
+      }
+      return fetch(url,options).then( response => response.json());
+
+   }
+
+   read(id){
+      return this.query(
+         `${this.options.object}/${id}`, 
+         {
+            method: 'GET'
+         })
+         .then(result => {
+            return (this.toModel(result));
+         });
+   }
+
+   create(data){
+      return this.query(
+         `${this.options.object}/${id}`, 
+         {  method: 'POST',
+            body:data
+      });
+   }
+
+   delete(id){
+      return this.query(
+         `${this.options.object}/${id}`, 
+         {method: 'DELETE'});
+   }
+
+   update(id,data){
+      return this.query(
+         `${this.options.object}/${id}`, 
+         {  method: 'PATCH',
+            body:data
+      });
+   }
+   list(page,limit){
+      return this.query(
+         `${this.options.object}`,
+         {
+            method:'GET'
+         },
+         {
+            '_page':page,
+            '_limit':limit
+         }).
+         then(results=>{
+            return   results.map((result) => this.toModel(result));
+         });
+   }
+
 }
 
-
+let dataset = new DataSet({
+   object:"person",
+   model:Model
+});
+  // console.log("dataset.read(1).fullName")
+  // console.log(dataset.read(1).get_fullName())
+  let model1 = dataset.read(1);
+//alert( toString.call(model1) );
+list.add(dataset.read(1));
+list.add(dataset.read(2));
+list.add(dataset.read(3));
+list.add(dataset.read(4));
+list.add(dataset.read(5));
